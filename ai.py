f33aa18ec9a8ae24ff3af011ecd91e6f61537e1b
jiveshkalra <127482713+jiveshkalra@users.noreply.github.com>
2023-08-08 09:57:56 +0530

    ai model

--- ai.py
+++ ai.py
@@ -1,7 +1,34 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-# Use a pipeline as a high-level helper
+# T5-3b and T5-11B are supported!
+# We need sharded weights otherwise we get CPU OOM errors
+model_id=f"OpenAssistant/falcon-7b-sft-top1-696"
 
-from transformers import pipeline
+tokenizer = AutoTokenizer.from_pretrained(model_id,trust_remote_code=True)
+model_8bit = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", load_in_8bit=True,trust_remote_code=True)
 
-pipe = pipeline("text-generation", model="meta-llama/Llama-2-7b-hf",use_auth_token=True)
-print(pipe("Hey"))
+max_new_tokens = 250
+print("-----------------------------------------------------------------------")
+print("Good Morning Sir,")
+print("I am your personal mental health chatbot")
+print("You can ask me any question of your related to any kind of problem of yours")
+print("Please keep the prompt detailed about what you really want")
+print("------------------------------------------------------------------------")
+query = input("User: ")
+while True:
+  print("\n\n")
+  input_data = tokenizer(f"User: {query} \nChatbot:", return_tensors="pt")
+  input_ids = input_data.input_ids
+  attention_mask = input_data.attention_mask
+
+  outputs = model_8bit.generate(input_ids, max_new_tokens=max_new_tokens,attention_mask=attention_mask)
+  print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+  query = input("User: ")
+  if query == "quit":
+    print("\n\n")
+    print("----------------------------------")
+    print("It was nice talking to you")
+    print("Have A Nice day ahead!")
+    print("----------------------------------")
+    break
